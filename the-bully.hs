50a1a5a84dbf2d88afce33fcc222b130058d0bfd
Andy Morris <andy@adradh.org.uk>
2013-11-05 22:04:27 +0100

    the-bully: apply `seq` to the right thing
    Before, it was evaluating all the elements to WHNF, which isn't too useful for calculating the length. :)

--- the-bully.hs
+++ the-bully.hs
@@ -1,5 +1,4 @@
-
 -- | the bully takes the unassuming Prelude length and
 -- | coerces it to evaluate (+) as soon as possible
 bully [] = 0
-bully (x:xs) = x `seq` 1 + bully xs
+bully (x:xs) = let len = bully xs in len `seq` 1 + len
